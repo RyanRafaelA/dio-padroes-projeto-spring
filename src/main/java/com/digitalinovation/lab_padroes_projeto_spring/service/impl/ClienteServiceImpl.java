573febcb98e0b10bd9e5a96df3be1fa64c721abe
RyanRafaelA <ryrafael461@gmail.com>
2025-03-22 10:19:37 -0300

    Acertando a versão do Swagger e implementada o ClienteServiceImpl

---
+++ src/main/java/com/digitalinovation/lab_padroes_projeto_spring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,41 @@
+package com.digitalinovation.lab_padroes_projeto_spring.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.digitalinovation.lab_padroes_projeto_spring.model.Cliente;
+import com.digitalinovation.lab_padroes_projeto_spring.service.ClienteService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletar(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
